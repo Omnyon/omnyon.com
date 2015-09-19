d6e9dfc9ce567ac79cd7c2ac15a69d886d41a567
jeffshaver <jeff.e.shaver@gmail.com>
2015-09-18 20:53:11 -0400

    Change eslintrc. Change gulpfile. Change styles. Add hash support for real. Fix some issues with padding

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
 var scss = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('babel', function() {
   return gulp.src('js/**/*.js')
-    .pipe(babel())
+    .pipe(sourcemaps.init())
+    .pipe(babel({stage: 0}))
+    // .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('scss', function() {
   return gulp.src('css/index.scss')
-    .pipe(scss().on('error', scss.logError))
+    .pipe(scss({outputStyle: 'compressed'}).on('error', scss.logError))
     .pipe(autoprefixer())
     .pipe(gulp.dest('dist'));
 });
--- js/index.js
+++ js/index.js
@@ -1,31 +1,80 @@
-(function auto() {
+(function() {
   'use strict';
+  let QS = document::document.querySelector;
   let nav = document.querySelector('nav');
   let navHeight = nav.clientHeight;
-  let changeHeaderColor = function changeHeaderColor() {
-    if (window.scrollY > 0) {
-      nav.classList.add('scrolled');
-    } else {
-      nav.classList.remove('scrolled');
+  // shim layer with setTimeout fallback
+  let requestAnimFrame = (function() {
+    return window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      function(callback) {
+        window.setTimeout(callback, 1000 / 60);
+      };
+  })();
+
+  let handleScroll = function handleScroll() {
+    changeHeaderColor(window.scrollY > 0);
+  };
+  let changeHeaderColor = function changeHeaderColor(isScrolled) {
+    if (isScrolled && nav.classList.contains('scrolled')) {
+      return;
     }
+    nav.classList[isScrolled ? 'add' : 'remove']('scrolled');
+  };
+  let getHash = function getHash() {
+    return window.location.hash.substring(1);
+  };
+  let setHash = function setHash(hash) {
+    window.location.hash = hash.substring(1);
+  };
+  let getTopOfElement = function getTopOfElement(el) {
+    return el.getBoundingClientRect().top + window.scrollY - navHeight;
+  };
+  let scrollToElement = function scrollToElement(selector, time) {
+    let el = document.querySelector(selector);
+    let topOfEl = getTopOfElement(el);
+
+    TweenMax.to(window, time, {
+      scrollTo: {
+        y: topOfEl,
+        autoKill: true
+      }
+    });
+  };
+  let handleResize = function handleResize() {
+    if (window.innerWidth > 850) {
+      nav.classList.remove('open');
+    }
+  };
+  let debounce = function debounce(func, wait, immediate) {
+    let timeout;
+
+    return function debounced() {
+      let context = this;
+      let args = arguments;
+      let later = function later() {
+        timeout = null;
+        if (!immediate) {
+          func.apply(context, args);
+        }
+      };
+      let callNow = immediate && !timeout;
+
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) {
+        func.apply(context, args);
+      }
+    };
   };
 
   [...document.querySelectorAll('[data-scroll-to]')].forEach(function forEachDataScrollTo(el) {
     el.addEventListener('click', function onClick(ev) {
       ev.preventDefault();
-      window.location.hash = ev.target.getAttribute('href').substring(1);
-
-      let topY = document.querySelector(this.getAttribute('data-scroll-to')).getBoundingClientRect().top + window.scrollY - navHeight;
-
+      setHash(ev.target.getAttribute('href'));
       nav.classList.remove('open');
-
-      TweenMax.to(window, 1, {
-        scrollTo: {
-          y: topY,
-          autoKill: true
-        },
-        ease: Power3.easeOut
-      });
+      scrollToElement(ev.target.getAttribute('data-scroll-to'), 1);
     });
   });
 
@@ -34,7 +83,18 @@
     nav.classList.toggle('open');
   });
 
-  changeHeaderColor();
+  if (getHash()) {
+    requestAnimFrame(function() {
+      window.scrollTo(0, getTopOfElement(QS('.js-' + getHash())));
+      handleScroll();
+    });
+  } else {
+    handleScroll();
+  }
 
-  window.addEventListener('scroll', changeHeaderColor);
+  window.addEventListener('scroll', handleScroll);
+  window.addEventListener('resize', debounce(handleResize, 1000));
+  window.addEventListener('beforeunload', function() {
+    window.scrollTo(0, getTopOfElement(QS('.js-' + getHash())));
+  });
 }());
