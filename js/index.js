54246ed6be422b223467ab237af5f43eda6d815a
jeffshaver <jeff.e.shaver@gmail.com>
2015-09-23 19:18:28 -0400

    Change how the JS is structured

--- js/index.js
+++ js/index.js
@@ -1,100 +1,107 @@
 (function() {
   'use strict';
-  let QS = document::document.querySelector;
-  let nav = document.querySelector('nav');
-  let navHeight = nav.clientHeight;
-  // shim layer with setTimeout fallback
-  let requestAnimFrame = (function() {
-    return window.requestAnimationFrame ||
-      window.webkitRequestAnimationFrame ||
-      window.mozRequestAnimationFrame ||
-      function(callback) {
-        window.setTimeout(callback, 1000 / 60);
-      };
-  })();
-
-  let handleScroll = function handleScroll() {
-    changeHeaderColor(window.scrollY > 0);
-  };
-  let changeHeaderColor = function changeHeaderColor(isScrolled) {
-    if (isScrolled && nav.classList.contains('scrolled')) {
-      return;
-    }
-    nav.classList[isScrolled ? 'add' : 'remove']('scrolled');
-  };
-  let getHash = function getHash() {
-    return window.location.hash.substring(1);
-  };
-  let setHash = function setHash(hash) {
-    window.location.hash = hash.substring(1);
-  };
-  let getTopOfElement = function getTopOfElement(el) {
-    return el.getBoundingClientRect().top + window.scrollY - navHeight;
-  };
-  let scrollToElement = function scrollToElement(selector, time) {
-    let el = document.querySelector(selector);
-    let topOfEl = getTopOfElement(el);
-
-    TweenMax.to(window, time, {
-      scrollTo: {
-        y: topOfEl,
-        autoKill: true
+  let App = {
+    QS: document::document.querySelector,
+    QSA: document::document.querySelectorAll,
+    nav: null,
+    navHeight: null,
+    requestAnimationFrame: window::(function() {
+      return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        function(callback) {
+          window.setTimeout(callback, 1000 / 60);
+        };
+    }()),
+    init() {
+      if (App.getHash()) {
+        App.requestAnimationFrame(function() {
+          window.scrollTo(0, App.getTopOfElement(App.QS('.js-' + App.getHash())));
+          App.handleScroll();
+        });
+      } else {
+        App.handleScroll();
       }
-    });
-  };
-  let handleResize = function handleResize() {
-    if (window.innerWidth > 850) {
-      nav.classList.remove('open');
-    }
-  };
-  let debounce = function debounce(func, wait, immediate) {
-    let timeout;
+      App.attachListeners();
+    },
+    attachListeners() {
+      App.nav = App.QS('nav');
+      App.navHeight = App.nav.clientHeight;
+      [...App.QSA('[data-scroll-to]')].forEach(function(element) {
+        element.addEventListener('click', App.handleScrollToClick);
+      });
+      App.QS('.nav-button').addEventListener('click', function(ev) {
+        ev.preventDefault();
+        App.nav.classList.toggle('open');
+      });
+      window.addEventListener('scroll', App.handleScroll);
+      window.addEventListener('resize', App.debounce(App.handleResize), 1000);
+      window.addEventListener('beforeunload', App.handleBeforeUnload);
+    },
+    handleScroll() {
+      App.changeHeaderColor(window.scrollY > 0);
+    },
+    changeHeaderColor(isScrolled = false) {
+      if (isScrolled && App.nav.classList.contains('scrolled')) {
+        return;
+      }
+      App.nav.classList[isScrolled ? 'add' : 'remove']('scrolled');
+    },
+    getHash() {
+      return window.location.hash.substring(1);
+    },
+    setHash(hash) {
+      window.location.hash = hash.substring(1);
+    },
+    getTopOfElement(element) {
+      return element.getBoundingClientRect().top + window.scrollY - App.navHeight;
+    },
+    scrollToElement(selector, time) {
+      let el = App.QS(selector);
+      let topOfElement = App.getTopOfElement(el);
 
-    return function debounced() {
-      let context = this;
-      let args = arguments;
-      let later = function later() {
-        timeout = null;
-        if (!immediate) {
+      TweenMax.to(window, time, {
+        scrollTo: topOfElement,
+        autoKill: true
+      });
+    },
+    handleScrollToClick(ev) {
+      ev.preventDefault();
+      App.setHash(ev.target.getAttribute('href'));
+      App.nav.classList.remove('open');
+      App.scrollToElement(ev.target.getAttribute('data-scroll-to'), 1);
+    },
+    handleResize() {
+      if (window.innerWidth < 850) {
+        return;
+      }
+      App.nav.classList.remove('open');
+    },
+    handleBeforeUnload() {
+      window.scrollTo(0, App.getTopOfElement(App.QS('.js-' + getHash())));
+    },
+    debounce(func, wait, immediate) {
+      let timeout;
+
+      return function debounced() {
+        let context = this;
+        let args = arguments;
+        let later = function later() {
+          timeout = null;
+          if (!immediate) {
+            func.apply(context, args);
+          }
+        };
+        let callNow = immediate && !timeout;
+
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) {
           func.apply(context, args);
         }
       };
-      let callNow = immediate && !timeout;
-
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) {
-        func.apply(context, args);
-      }
-    };
+    }
   };
 
-  [...document.querySelectorAll('[data-scroll-to]')].forEach(function forEachDataScrollTo(el) {
-    el.addEventListener('click', function onClick(ev) {
-      ev.preventDefault();
-      setHash(ev.target.getAttribute('href'));
-      nav.classList.remove('open');
-      scrollToElement(ev.target.getAttribute('data-scroll-to'), 1);
-    });
-  });
-
-  document.querySelector('.nav-button').addEventListener('click', function navClick(ev) {
-    ev.preventDefault();
-    nav.classList.toggle('open');
-  });
-
-  if (getHash()) {
-    requestAnimFrame(function() {
-      window.scrollTo(0, getTopOfElement(QS('.js-' + getHash())));
-      handleScroll();
-    });
-  } else {
-    handleScroll();
-  }
-
-  window.addEventListener('scroll', handleScroll);
-  window.addEventListener('resize', debounce(handleResize, 1000));
-  window.addEventListener('beforeunload', function() {
-    window.scrollTo(0, getTopOfElement(QS('.js-' + getHash())));
-  });
+  App.init();
 }());
