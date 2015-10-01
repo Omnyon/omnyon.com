db83f3281432bb0099d07a99fc5c20e4e0b93888
jeffshaver <jeff.e.shaver@gmail.com>
2015-09-30 21:56:55 -0400

    Fix slants code and make it more performant

--- js/slants.js
+++ js/slants.js
@@ -1,4 +1,7 @@
+import {QSA} from './qs';
+
 let masks = [];
+const MASK_HEIGHT = 45;
 
 export function drawMasks() {
   if (masks.length === 0) {
@@ -13,45 +16,57 @@ function createMasks() {
 
   top.classList.add('section-mask', 'js-section-mask', 'reversed');
   bottom.classList.add('section-mask', 'js-section-mask');
+  top.height = bottom.height = MASK_HEIGHT;
   return [top, bottom];
 }
 
-Array.prototype.slice.call(document.querySelectorAll('section')).forEach(function(section, i) {
+[...QSA('section')].forEach(function(section, i) {
   let [top, bottom] = createMasks();
 
-  section.appendChild(bottom);
-  if (i !== 0) {
-    section.insertBefore(top, section.children[0]);
+  if (i === 0) {
+    section.appendChild(bottom);
+    masks.push({section, bottom});
+    return;
   }
-  masks.push({
-    section,
-    bottom,
-    top
-  });
+
+  if (i !== 0 && i % 2 === 0) {
+    section.appendChild(bottom);
+    section.insertBefore(top, section.children[0]);
+    masks.push({section, bottom, top});
+  }
 });
 
 function drawMask({section, bottom, top}) {
-  let topContext = top.getContext('2d');
-  let bottomContext = bottom.getContext('2d');
-  let height = top.height = bottom.height = 45;
-  let width = top.width = bottom.width = section.clientWidth;
-
-  drawTopMask(topContext, height, width);
-  drawBottomMask(bottomContext, height, width);
+  if (top) {
+    drawTopMask(top, section);
+  }
+  if (bottom) {
+    drawBottomMask(bottom, section);
+  }
 }
 
-function drawTopMask(context, height, width) {
+function setupContext(mask, section) {
+  let context = mask.getContext('2d');
+  let width = mask.width = section.clientWidth;
+
   context.fillStyle = 'hsl(191, 69%, 92%)';
-  context.moveTo(0, height);
+  return [context, width];
+}
+
+function drawTopMask(mask, section) {
+  let [context, width] = setupContext(mask, section);
+
+  context.moveTo(0, MASK_HEIGHT);
   context.lineTo(0, 0);
   context.lineTo(width, 0);
   context.fill();
 }
 
-function drawBottomMask(context, height, width) {
-  context.fillStyle = 'hsl(191, 69%, 92%)';
-  context.moveTo(0, height);
-  context.lineTo(width, height);
+function drawBottomMask(mask, section) {
+  let [context, width] = setupContext(mask, section);
+
+  context.moveTo(0, MASK_HEIGHT);
+  context.lineTo(width, MASK_HEIGHT);
   context.lineTo(width, 0);
   context.fill();
 }
